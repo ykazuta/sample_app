2f324f8388bad8abe872093f62933dd73149d1bb
Yusuke Kazuta <y_kazuta@learnie-inc.com>
2018-11-28 15:12:10 +0900

    Initial Commit

---
+++ sample_app/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sample_app
+//
+//  Created by 數田裕介 on 2018/11/28.
+//  Copyright © 2018年 LEARNie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ sample_app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sample_app
+//
+//  Created by 數田裕介 on 2018/11/28.
+//  Copyright © 2018年 LEARNie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
